f7e6c2795bfd7ce2695d99ea0ef2e73be3219e37
Ilayda Guzel <ilaydanurguzel1@gmail.com>
2023-03-24 13:36:32 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+
+//1
+var customerName='bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+};
+
+
+
+
+//2
+function setBestCustomer(){
+    bestCustomer="not bob";
+};
+
+
+//3
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+};
+
+
+//4
+const leastFavoriteCustomer="Elif";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Ipek"
+};
+
+
+
